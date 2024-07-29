5698773115063bc53ceac376ed7efd4b41204d92
miku <2250694@tongji.edu.cn>
2024-07-29 16:27:08 +0800

    lab 6.3 finished

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -25,12 +25,27 @@ barrier_init(void)
 static void 
 barrier()
 {
-  // YOUR CODE HERE
-  //
-  // Block until all threads have called barrier() and
-  // then increment bstate.round.
-  //
-  
+    // 加锁以保护对共享状态的访问
+    pthread_mutex_lock(&bstate.barrier_mutex);
+
+    // 增加已到达此轮次的线程数量
+    bstate.nthread++;
+
+    // 如果还没有所有线程到达屏障，则等待
+    if (bstate.nthread < nthread) {
+        pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+    }
+
+    // 如果所有线程都已到达屏障，增加轮次并重置计数器
+    if (bstate.nthread == nthread) {
+        bstate.round++;
+        bstate.nthread = 0;
+        // 通知所有等待的线程
+        pthread_cond_broadcast(&bstate.barrier_cond);
+    }
+
+    // 解锁
+    pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
